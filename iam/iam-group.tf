677fbcdee110121a6a3f0c00c67b452bac45e967
nao_e <nao_e@nao-laptop.localdomain>
2023-04-16 17:21:42 +0900

    iamのリファクタリング

--- iam/iam-group.tf
+++ iam/iam-group.tf
@@ -6,3 +6,11 @@ resource "aws_iam_group_policy_attachment" "power_user_attach" {
   group      = "power-user"
   policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
 }
+
+resource "aws_iam_group_membership" "power_user" {
+  name = "power-user-membership"
+  users = [
+    aws_iam_user.develop.name
+  ]
+  group = aws_iam_group.power_user.name
+}
